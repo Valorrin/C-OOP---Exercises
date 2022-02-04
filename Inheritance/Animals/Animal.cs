e694148c5f0b71ea43d3d1465049fff6723f4dfc
Valorrin <valorrin@gmail.com>
2022-02-04 17:12:40 +0200

    Completed Task: Animals

---
+++ Inheritance/Animals/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public virtual string Sound { get; set; }
+
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string ProduceSound()
+        {
+            return Sound;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine($"{ProduceSound()}");
+
+            return sb.ToString().Trim();
+        }
+
+
+    }
+}
--- Inheritance/Animals/StartUp.cs
+++ Inheritance/Animals/StartUp.cs
@@ -1,10 +1,64 @@
-﻿namespace Animals
+﻿using System;
+using System.Text;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            var sb = new StringBuilder();
+
+            while (true)
+            {
+                string animalType = Console.ReadLine();
+
+                if (animalType == "Beast!")
+                {
+                    break;
+                }
+
+                string[] parts = Console.ReadLine().Split(' ');
+                string name = parts[0];
+                int age = int.Parse(parts[1]);
+                string gender = parts[2];
+
+
+                if (age < 0 || (gender != "Male" && gender != "Female"))
+                {
+                    sb.AppendLine("Invalid input!");
+                    continue;
+                }
+
+                if (animalType == "Dog")
+                {
+                    var dog = new Dog(name, age, gender);
+                    sb.AppendLine(dog.ToString());
+                }
+                else if (animalType == "Cat")
+                {
+                    var cat = new Cat(name, age, gender);
+                    sb.AppendLine(cat.ToString());
+                }
+                else if (animalType == "Frog")
+                {
+                    var frog = new Frog(name, age, gender);
+                    sb.AppendLine(frog.ToString());
+                }
+                else if (animalType == "Kitten")
+                {
+                    var kitten = new Kitten(name, age);
+                    sb.AppendLine(kitten.ToString());
+                }
+                else if (animalType == "Tomcat")
+                {
+                    var tomcat = new Tomcat(name, age);
+                    sb.AppendLine(tomcat.ToString());
+                }
+
+            }
+
+            Console.WriteLine(sb.ToString());
         }
     }
 }
